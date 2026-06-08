a8eda65bce4853e43f6c505757a64ce99cd3b071
Stefano Frontini <stefanofrontini75@gmail.com>
2026-06-08 16:37:28 +0200

    fix11

--- src/components/Numeri/shared/constants.ts
+++ src/components/Numeri/shared/constants.ts
@@ -1,4 +1,4 @@
 export const curYear = new Date().getFullYear();
 export const firstYear = curYear - 2;
 export const url =
-  "https://gist.githubusercontent.com/StefanoFrontini/1572aa83bd623e27a7ff35a917872203/raw/ac2333946a6c9d1d78b4c0b35c336ebc1db76e88/sezione2-test.json";
+  "https://gist.githubusercontent.com/StefanoFrontini/1572aa83bd623e27a7ff35a917872203/raw/fdda709e0b99fc5d5117a6260ead81f840da9360/sezione2-test.json";
